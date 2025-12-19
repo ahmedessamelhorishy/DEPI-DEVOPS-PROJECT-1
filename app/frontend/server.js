c0c512a60168d6ec51330722cd4a81479e912766
Ahmed Essam <ahmedessam91@hotmail.com>
2025-12-19 14:55:52 +0200

    Adjust frotend server.js file

--- app/frontend/server.js
+++ app/frontend/server.js
@@ -8,10 +8,11 @@ app.get('/api', async (req, res) => {
   try {
     const response = await fetch('http://backend:4000/data');
     const text = await response.text();
-    res.send("Backend says: " + text);
+    res.send("Backend says now: " + text);
   } catch (err) {
     res.send("Error contacting backend: " + err.message);
   }
 });
 
 app.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
+
